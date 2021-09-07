94aaf2a857060498a0c2530accc403eec0547b2a
Glenn Svanberg <gsg@fiwe.local>
2021-09-07 18:19:37 +0200

    Print stations in infinite loop

--- okq8.py
+++ okq8.py
@@ -2,25 +2,46 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 driver = webdriver.Chrome('./chromedriver')
+
+def printStations(rows):
+    for row in rows:
+        name= row.find_element_by_class_name("station-name")
+        print("NAME:",name.text)
+        adress_rows = row.find_elements_by_class_name("globalMap-address")
+        adress = ""
+        for row in adress_rows:
+            if adress != "":
+                adress= adress + " " + row.text
+            else: 
+                adress = row.text
+        print("Adress:",adress)
+
 driver.get("https://www.okq8.se/pa-stationen/bensinstationer/")
-print(driver.title)
 
 elem = driver.find_element_by_class_name("coi-banner__accept").click()
-
-
 fuel_list = driver.find_element_by_partial_link_text('Drivmedel')
 
 fuel_list.click()
 
 span = fuel_list.find_element_by_xpath('//span[text()="GoEasy 98 Extra (E5)"]')
-print(span.text)
-#actions = ActionChains(driver)
-#actions.move_to_element(span).perform()
-print("moved")
 driver.execute_script("arguments[0].scrollIntoView();", span)
 driver.execute_script("arguments[0].click();", span)
-map = driver.find_element_by_class_name("globalMap-canvas")
-map.send_keys(Keys.SHIFT,Keys.SUBTRACT)
-map.send_keys(Keys.SHIFT,Keys.SUBTRACT)
-map.send_keys(Keys.SHIFT,Keys.SUBTRACT)
-# Markera karta och klicka shift minus för att zooma
+
+control = driver.find_element_by_xpath('//button[@title="Zooma ut"]')
+
+for i in range(15):
+    control.click()
+
+driver.maximize_window()
+driver.find_element_by_class_name('link-arrow').click()
+
+
+table = driver.find_element_by_class_name("globalMap-table-body")
+rows = table.find_elements_by_class_name("globalMap-table-row")
+printStations(rows)
+while True:
+    next_button = driver.find_element_by_class_name("globalMap-see-more")
+    driver.execute_script("arguments[0].scrollIntoView();", next_button)
+    next_button.click()
+    rows = table.find_elements_by_class_name("globalMap-table-row")
+    printStations(rows)
